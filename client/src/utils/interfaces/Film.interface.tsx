a3cdf0f26fec77689e4a0e95161fd66aaeb52057
Hannah Bryant <hannahbry05@gmail.com>
2025-02-04 22:36:08 -0700

    interfaces and title

---
+++ client/src/utils/interfaces/Film.interface.tsx
@@ -0,0 +1,11 @@
+export default interface Film {
+  readonly Title: string | null;
+  readonly Director: string | null;
+  readonly Actors: string | null;
+  readonly Released: string | null;
+  readonly Poster: string | null;
+  readonly Genre: string | null;
+  readonly Plot: string | null;
+}
+
+// This is just to have something, I'll change it with correct info later
